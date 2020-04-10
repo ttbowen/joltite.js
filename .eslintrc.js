f28a7c2c2d4ba996cb3401ed27b3f46683a8913e
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2020-04-10 16:50:28 +0100

    Update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint"
-  ]
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
 };
