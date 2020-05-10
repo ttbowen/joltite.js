c076b49898e05bb72a674130fb136232b9fd72a9
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2020-05-11 00:24:01 +0100

    Change format to iife

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,11 @@ const { version } = require('./package.json');
 
 export default {
   input: './dist/index.js',
-
   output: {
     file: `./rollup/joltite-${version}.js`,
     name: 'joltite',
+    format: 'iife',
     preferConst: true,
   },
-
   onwarn() {},
 };
